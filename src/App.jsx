28b4346c527e1ce1f671ed96579648b7ed9d0b36
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-14 17:50:04 +0100

    Trabajando en el componente Resultado y css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable react/prop-types */
-import Botones from "./componentes/Boton";
+
+import Resultados from "./componentes/Resultados";
 
 function App() {
   return (
     <div className="btn-container">
-      <Botones color={"acento"} texto={"Abrir"} />
-      <Botones color={"invertido"} texto={"Guardar"} />
-      <Botones color={"base"} texto={"Eliminar"} />
+      <Resultados />
     </div>
   );
 }
---
+++ src/componentes/Resultados.jsx
@@ -0,0 +1,63 @@
+import "./Resultados.css";
+
+export default function Resultados() {
+  return (
+    <main>
+      <section className="card">
+        <h2>Su Resultado</h2>
+        <div className="circle">
+          <h1>76</h1>
+          <p>de 100</p>
+        </div>
+        <h1>Optimo</h1>
+        <p>
+          Su resultado es mejor que el 65% de las personas que hicieron esta
+          prueba.
+        </p>
+      </section>
+
+      <section className="sumario">
+        <h1>Sumario</h1>
+
+        <div className="estrellas-red">
+          <span className="estrellas-color-red">
+            <img
+              src="assets/imagenes/icon-reaction.svg"
+              alt="Ícono de reaccion"
+            />
+            Reacción
+          </span>
+          <span className="fraccion">80 / 100</span>
+        </div>
+
+        <div className="estrellas-yellow">
+          <span className="estrellas-color-yellow">
+            <img src="assets/imagenes/icon-memory.svg" alt="Ícono de memoria" />
+            Memoria
+          </span>
+          <span className="fraccion">92 / 100</span>
+        </div>
+
+        <div className="estrellas-green">
+          <span className="estrellas-color-green">
+            <img src="assets/imagenes/icon-verbal.svg" alt="Ícono de chatbox" />
+            Verbal
+          </span>
+          <span className="fraccion">61 / 100</span>
+        </div>
+
+        <div className="estrellas-blue">
+          <span className="estrellas-color-blue">
+            <img src="assets/imagenes/icon-visual.svg" alt="Ícono de ojo" />
+            Visual
+          </span>
+          <span className="fraccion">72 / 100</span>
+        </div>
+
+        <button>
+          <a href="#">Continuar</a>
+        </button>
+      </section>
+    </main>
+  );
+}

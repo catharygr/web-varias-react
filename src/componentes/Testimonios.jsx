2c224ec2270f5e9a9492ecfe855130bb8a08ee50
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-18 17:31:37 +0100

    Cambiando la página a estática a dinámica

--- src/componentes/Testimonios.jsx
+++ src/componentes/Testimonios.jsx
@@ -6,7 +6,7 @@ export default function Testimonios() {
       id: 1,
       nombre: "Daniel Clifford",
       trabajo: "Graduado verificado",
-      testimonio:
+      testimonial:
         "Recibí una oferta de trabajo en medio del curso, y las asignaturas que aprendí eran actuales, si no más, en la empresa a la que me uní. Honestamente, siento que valió cada centavo.",
       descriccion:
         "Fui técnico de emergencias médicas (EMT) durante muchos años antes de unirme al bootcamp. Estaba buscando hacer una transición y había escuchado a algunas personas que tuvieron una experiencia increíble aquí. Me inscribí en el curso introductorio gratuito y ¡lo encontré increíblemente divertido! Poco después me matriculé. Las siguientes 12 semanas fueron el mejor -y más agotador- momento de mi vida. Desde que completé el curso, he logrado cambiar de carrera exitosamente y ahora trabajo como Ingeniero de Software en una startup de realidad virtual.",
@@ -17,7 +17,7 @@ export default function Testimonios() {
       id: 2,
       nombre: "Jonathan Walters",
       trabajo: "Graduado verificado",
-      testimonio: "El equipo fue muy solidario y me mantuvo motivado.",
+      testimonial: "El equipo fue muy solidario y me mantuvo motivado.",
       descriccion:
         "Comencé como un novato total con prácticamente ninguna habilidad de programación. Ahora trabajo como ingeniero móvil para una gran empresa. Esta fue una de las mejores inversiones que he hecho en mí mismo.",
       imagen: "/assets/imagenes/testimonios/image-jonathan.jpg",
@@ -27,7 +27,7 @@ export default function Testimonios() {
       id: 3,
       nombre: "Kira Whittle",
       trabajo: "Graduado verificado",
-      testimonio:
+      testimonial:
         "¡Una experiencia que cambia la vida por completo! ¡Altamente recomendado!",
       descriccion:
         "Antes de unirme al bootcamp, nunca había escrito una línea de código. Necesitaba una estructura proporcionada por profesionales que pudieran ayudarme a aprender programación paso a paso. Me animaron a inscribirme a través de un antiguo estudiante suyo que solo podía decir cosas maravillosas sobre el programa. Todo el plan de estudios y el personal no defraudaron. Fueron muy prácticos y nunca tuve que esperar mucho tiempo para recibir ayuda. El proyecto del equipo ágil, en particular, fue excepcional. Llevó mi aprendizaje al siguiente nivel de una manera que ningún tutorial podría haber logrado. De hecho, a menudo lo he mencionado en entrevistas como ejemplo de mi experiencia en desarrollo. Sin duda, me ayudó a conseguir un trabajo como desarrollador full-stack después de recibir múltiples ofertas. ¡Recomendado al 100%!",
@@ -38,7 +38,7 @@ export default function Testimonios() {
       id: 4,
       nombre: "Jeanette Harmon",
       trabajo: "Graduado verificado",
-      testimonio:
+      testimonial:
         "El programa fue muy desafiante, pero lo apoyaron todos los días para que tuviera éxito.",
       descriccion:
         "Me encantó cómo se estructuró el programa. No hay nada como esto en el mercado. El currículo y el plan de estudios son increíbles, y estoy muy feliz de haberme inscrito. ¡Los proyectos que hice con mi equipo fueron tan buenos que los incluí en mi cartera profesional! ¡Esto me ayudó a conseguir mi trabajo actual como desarrollador de software full-stack!",
@@ -61,13 +61,18 @@ export default function Testimonios() {
   return (
     <main className="container">
       {testimoniosData.map((testimonio) => (
-        <div className="tarjetas" key={testimonio.id}>
+        <div className={`tarjetas ${testimonio.id}`} key={testimonio.id}>
           <img
             src="/assets/imagenes/testimonios/bg-pattern-quotation.svg"
             className="comillas"
+            alt="Comillas"
           />
           <div className="header">
-            <img className="miniatura" src={testimonio.imagen} />
+            <img
+              className="miniatura"
+              src={testimonio.imagen}
+              alt="Foto de perfil"
+            />
             <div className="personal-info">
               <p className="nombre-apellido">{testimonio.nombre}</p>
               <p className="trabajo">{testimonio.trabajo}</p>

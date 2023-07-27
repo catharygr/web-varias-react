711e5c9cc4c267a7742ffc4471048058b11cf12e
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-27 18:59:31 +0100

    Trabajando con el css

--- src/componentes/ChatApp.jsx
+++ src/componentes/ChatApp.jsx
@@ -4,76 +4,103 @@ export default function ChatApp() {
   return (
     <>
       <main>
-        <section className="telefono-seccion">
-          <div className="app-header">
-            <img
-              src="/assets/imagenes/chatapp/avatar.jpg"
-              alt="Foto de perfil"
-            />
-            <div className="perfiles">
-              <span className="perfiles-nombre">Samuel Green</span>
-              <span style={{ display: "block" }}>Disponible para pasear</span>
+        <section className="container">
+          <div className="primera-tarjeta"></div>
+          <div className="segunda-tarjeta">
+            <div className="display-movil">
+              <div className="contenido-interno">
+                <div className="arco-medio"></div>
+
+                <ul>
+                  <li className="flecha-izq"></li>
+                  <li>
+                    {" "}
+                    <img
+                      src="/assets/imagenes/chatapp/avatar.jpg"
+                      alt="Imagen del avatar"
+                    />
+                  </li>
+                  <li className="primer-encabezado">
+                    <h3> Samuel Green</h3>
+                    <p>Available to Walk</p>
+                  </li>
+                  <li className="vl"></li>
+                </ul>
+
+                <p className="para-primero para-izq">
+                  {" "}
+                  That sounds great. I’d be happy with that.
+                </p>
+                <p className="para-segundo para-izq">
+                  {" "}
+                  Could you send over some pictures of your dog, please?
+                </p>
+                <div className="imagen-div">
+                  <img
+                    className="dog-image"
+                    src="/assets/imagenes/chatapp/dog-image-1.jpg"
+                    alt="dog-image-1"
+                  />
+                  <img
+                    className="dog-image"
+                    src="i/assets/imagenes/chatapp/dog-image-2.jpg"
+                    alt="dog-image-2"
+                  />
+                  <img
+                    className="dog-image"
+                    src="/assets/imagenes/chatapp/dog-image-3.jpg"
+                    alt="dog-image-3"
+                  />
+                </div>
+
+                <p className="para-derecho para-ancho-derecho">
+                  Here are a few pictures. She’s a happy girl!
+                </p>
+                <p className="para-derecho para-derecho-peque">
+                  {" "}
+                  Can you make it?
+                </p>
+
+                <p className="para-last para-left">
+                  {" "}
+                  She looks so happy! The time we discussed works. How long
+                  shall I take her out for?
+                </p>
+
+                <div className="circle up"></div>
+                <div className="circle down"></div>
+                <div>
+                  <p className="walk walk-first">
+                    <span className="minute">30 minute walk</span>{" "}
+                    <span className="dollar"> $29</span>
+                  </p>
+                  <p className="walk">
+                    {" "}
+                    <span className="minute">30 minute walk</span>{" "}
+                    <span className="dollar">$49</span>{" "}
+                  </p>
+                </div>
+
+                <p className="message-type">Type a message…</p>
+                <div className="summit-button">
+                  <i className="arrow right"></i>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="app-interfaz">
-            <span className="msg enviar-msg">
-              Eso suena genial. Estaría feliz con eso.
-            </span>
-            <span className="msg enviar-msg">
-              ¿Podrías enviarme algunas fotos de tu perro, por favor?
-            </span>
-            <span className="foto-msg">
-              <img
-                src="/assets/imagenes/chatapp/dog-image-1.jpg"
-                alt="Foto de perro"
-              />
-              <img
-                src="/assets/imagenes/chatapp/dog-image-2.jpg"
-                alt="Foto de perro"
-              />
-              <img
-                src="/assets/imagenes/chatapp/dog-image-3.jpg"
-                alt="Foto de perro"
-              />
-            </span>
-            <span className="msg receptor-msg">
-              Aquí tienes algunas fotos. ¡Ella es una perrita feliz!
-            </span>
-            <span className="msg receptor-msg">¿Puedes hacerlo?</span>
-            <span className="msg enviar-msg">
-              ¡Se ve tan feliz! El horario que discutimos funciona. ¿Cuánto
-              tiempo debo sacarla a pasear?
-            </span>
-            <span className="oferta oferta-1">
-              Paseo de 30 minutos.
-              <span className="precio">$29</span>
-            </span>
-            <span className="oferta oferta-2">
-              Paseo de 1 hora.
-              <span className="precio">$49</span>
-            </span>
-            <div className="mensaje-area">Escribe un mensaje...</div>
-          </div>
         </section>
-        <section className="texto-seccion">
-          <h1>Reserva sencilla.</h1>
-          <p>
-            Mantente en contacto con nuestros paseadores de perros a través de
-            la interfaz de chat. Esto facilita discutir arreglos y hacer
-            reservas. Una vez que se haya completado el paseo, puedes calificar
-            a tu paseador y reservar nuevamente, todo a través del chat.
+
+        <div className="container-second">
+          <h1> Simple booking</h1>
+
+          <p className="para-third">
+            {" "}
+            Stay in touch with our dog walkers through the chat interface. This
+            makes it easy to discuss arrangements and make bookings. Once the
+            walk has been completed you can rate your walker and book again all
+            through the chat.
           </p>
-          <footer className="atribucion">
-            <a
-              href="https://www.frontendmentor.io?ref=challenge"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Frontend Mentor
-            </a>
-            . Hecho por <a href="https://bubulazi.com/">bubulazi.com</a>.
-          </footer>
-        </section>
+        </div>
       </main>
     </>
   );

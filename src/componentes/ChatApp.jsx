699927e89d45c815a6a3fe4ee3933d3ceafdc10b
catharygr <103951447+catharygr@users.noreply.github.com>
2023-07-27 20:41:21 +0100

    Trabajando con el css

--- src/componentes/ChatApp.jsx
+++ src/componentes/ChatApp.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import "./ChatApp.css";
 
 export default function ChatApp() {
@@ -37,19 +38,19 @@ export default function ChatApp() {
                 </p>
                 <div className="imagen-div">
                   <img
-                    className="dog-image"
+                    className="dog-imagen"
                     src="/assets/imagenes/chatapp/dog-image-1.jpg"
-                    alt="dog-image-1"
+                    alt="Imagen de un perro"
                   />
                   <img
-                    className="dog-image"
+                    className="dog-imagen"
                     src="i/assets/imagenes/chatapp/dog-image-2.jpg"
-                    alt="dog-image-2"
+                    alt="Imagen de un perro"
                   />
                   <img
-                    className="dog-image"
+                    className="dog-imagen"
                     src="/assets/imagenes/chatapp/dog-image-3.jpg"
-                    alt="dog-image-3"
+                    alt="Imagen de un perro"
                   />
                 </div>
 
@@ -61,39 +62,39 @@ export default function ChatApp() {
                   Can you make it?
                 </p>
 
-                <p className="para-last para-left">
+                <p className="para-ultimo para-izq">
                   {" "}
                   She looks so happy! The time we discussed works. How long
                   shall I take her out for?
                 </p>
 
-                <div className="circle up"></div>
-                <div className="circle down"></div>
+                <div className="circulo subir"></div>
+                <div className="circulo bajar"></div>
                 <div>
-                  <p className="walk walk-first">
-                    <span className="minute">30 minute walk</span>{" "}
-                    <span className="dollar"> $29</span>
+                  <p className="pasear pasear-primero">
+                    <span className="minuto">30 minute walk</span>{" "}
+                    <span className="dolar"> $29</span>
                   </p>
-                  <p className="walk">
+                  <p className="pasear">
                     {" "}
-                    <span className="minute">30 minute walk</span>{" "}
-                    <span className="dollar">$49</span>{" "}
+                    <span className="minuto">30 minute walk</span>{" "}
+                    <span className="dolar">$49</span>{" "}
                   </p>
                 </div>
 
-                <p className="message-type">Type a message…</p>
-                <div className="summit-button">
-                  <i className="arrow right"></i>
+                <p className="tipo-mensaje">Type a message…</p>
+                <div className="enviar-btn">
+                  <i className="flecha derecho"></i>
                 </div>
               </div>
             </div>
           </div>
         </section>
 
-        <div className="container-second">
+        <div className="container-segundo">
           <h1> Simple booking</h1>
 
-          <p className="para-third">
+          <p className="para-tercero">
             {" "}
             Stay in touch with our dog walkers through the chat interface. This
             makes it easy to discuss arrangements and make bookings. Once the
@@ -101,6 +102,17 @@ export default function ChatApp() {
             through the chat.
           </p>
         </div>
+        <footer>
+          <div className="atribucion">
+            <a
+              href="https://www.frontendmentor.io?ref=challenge"
+              target="_blank"
+            >
+              Frontend Mentor
+            </a>
+            . hecho por <a href="https://bubulazi.com">bubulazi.com</a>.
+          </div>
+        </footer>
       </main>
     </>
   );

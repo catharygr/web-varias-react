6ceaf6bc1eebf97ff8da9266035d61ef869b6f1d
catharygr <103951447+catharygr@users.noreply.github.com>
2023-08-02 18:13:28 +0100

    Trabajando con css

--- src/componentes/ChatApp.jsx
+++ src/componentes/ChatApp.jsx
@@ -1,117 +1,3 @@
-/* eslint-disable react/jsx-no-target-blank */
-// import "./ChatApp.css";
-
-// export default function ChatApp() {
-//   return (
-//     <>
-//       <main>
-//         <div className="container-primero">
-//           <div className="primera-tarjeta"></div>
-//           <div className="segunda-tarjeta">
-//             <div className="display-movil">
-//               <div className="contenido-interno">
-//                 <div className="arco-medio"></div>
-
-//                 <ul>
-//                   <li className="flecha izq"></li>
-//                   <li>
-//                     {" "}
-//                     <img
-//                       src="/assets/imagenes/chatapp/avatar.jpg"
-//                       alt="Imagen del avatar"
-//                     />
-//                   </li>
-//                   <li className="primer-encabezado">
-//                     <h3> Samuel Green</h3>
-//                     <p>Disponibilidad para pasear</p>
-//                   </li>
-//                   <li className="vl"></li>
-//                 </ul>
-
-//                 <p className="para-primero para-izq">
-//                   {" "}
-//                   Eso suena genial. Estaría feliz con eso.
-//                 </p>
-//                 <p className="para-segundo para-izq">
-//                   {" "}
-//                   ¿Podrías enviarme algunas fotos de tu perro, por favor?
-//                 </p>
-//                 <div className="imagen-div">
-//                   <img
-//                     className="dog-imagen"
-//                     src="/assets/imagenes/chatapp/dog-image-1.jpg"
-//                     alt="Imagen de un perro"
-//                   />
-//                   <img
-//                     className="dog-imagen"
-//                     src="/assets/imagenes/chatapp/dog-image-2.jpg"
-//                     alt="Imagen de un perro"
-//                   />
-//                   <img
-//                     className="dog-imagen"
-//                     src="/assets/imagenes/chatapp/dog-image-3.jpg"
-//                     alt="Imagen de un perro"
-//                   />
-//                 </div>
-
-//                 <p className="para-derecho para-ancho-derecho">
-//                   Aquí tienes algunas fotos. ¡Ella es una perrita feliz!
-//                 </p>
-//                 <p className="para-derecho para-derecho-peque">
-//                   ¿Puedes hacerlo?
-//                 </p>
-
-//                 <p className="para-ultimo para-izq">
-//                   {" "}
-//                   ¡Se ve tan feliz! El horario que discutimos funciona. ¿Cuánto
-//                   tiempo debo sacarla a pasear?
-//                 </p>
-//                 <div className="circulo subir"></div>
-//                 <div className="circulo bajar"></div>
-//                 <div>
-//                   <p className="pasear pasear-primero">
-//                     <span>Paseo de 30 minutos.</span>{" "}
-//                     <span className="dolar"> $29</span>{" "}
-//                   </p>
-//                   <p className="pasear">
-//                     <span className="minuto">Paseo de 1 hora.</span>{" "}
-//                     <span className="dolar">$49</span>{" "}
-//                   </p>
-//                 </div>
-
-//                 <p className="tipo-mensaje">Escribe un mensaje...</p>
-//                 <div className="enviar-btn">
-//                   <i className="flecha derecho"></i>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="container-segundo"></div>
-//           <h1> Reserva sencilla</h1>
-
-//           <p className="para-tercero">
-//             {" "}
-//             Mantente en contacto con nuestros paseadores de perros a través de
-//             la interfaz de chat. Esto facilita discutir arreglos y hacer
-//             reservas. Una vez que se haya completado el paseo, puedes calificar
-//             a tu paseador y reservar nuevamente, todo a través del chat..
-//           </p>
-//         </div>
-//         {/* <footer>
-//           <div className="atribucion">
-//             <a
-//               href="https://www.frontendmentor.io?ref=challenge"
-//               target="_blank"
-//             >
-//               Frontend Mentor
-//             </a>
-//             . hecho por <a href="https://bubulazi.com">bubulazi.com</a>.
-//           </div>
-//         </footer> */}
-//       </main>
-//     </>
-//   );
-// }
 import "./ChatApp.css";
 export default function ChatApp() {
   return (
@@ -216,7 +102,11 @@ export default function ChatApp() {
       <footer>
         <div className="atribucion">
           {" "}
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
             Frontend Mentor
           </a>
           . hecho por <a href="https://bubulazi.com">bubulazi.com</a>.

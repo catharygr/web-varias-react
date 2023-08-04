ed82a9f1933973002411d8099c5e832923667cc3
catharygr <103951447+catharygr@users.noreply.github.com>
2023-08-04 18:11:38 +0100

    Trabajando con el componente Formulario y css

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -6,17 +6,35 @@ export default function Formulario() {
       <div className="formulario-loguear">
         <div className="formulario-contenido">
           <header>Iniciar sección</header>
-          <form action="">
-            <div className="formulario-input">
+          <form action="#">
+            <div className="amigo  input-amigo">
               <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="email" />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="email"
+                className="input"
+              />
             </div>
-            <div className="formulario-input">
+            <div className="amigo  input-amigo">
               <label htmlFor="password">Password</label>
               <input type="password" name="password" id="password" />
+              <i className="bx bx-hide fa-eye"></i>
+            </div>
+            <div className="form-link">
+              <a href="#" className="olvidaste-password">
+                ¿Has olvidado tu contraseña
+              </a>
             </div>
             <div className="formulario-input">
-              <button type="submit">Ingresar</button>
+              <button className="amigo btn-amigo">Iniciar sección</button>
+            </div>
+            <div className="form-link">
+              <span>No tiene una cuenta</span>
+              <a href="#" className="">
+                ¿Has olvidado tu contraseña
+              </a>
             </div>
           </form>
         </div>

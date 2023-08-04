9c601b3eede1f66aee18d3562f6e073fcb25cc97
catharygr <103951447+catharygr@users.noreply.github.com>
2023-08-04 18:47:46 +0100

    Trabajando con el componente Formulario y css

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -2,10 +2,11 @@ import "./Formulario.css";
 
 export default function Formulario() {
   return (
-    <section className="container-formulario">
-      <div className="formulario-loguear">
+    <section className="formulario">
+      {/* Inscribirse formulario */}
+      <div className="formulario-inscribirse">
         <div className="formulario-contenido">
-          <header>Iniciar sección</header>
+          <header>Inscribirse</header>
           <form action="#">
             <div className="amigo  input-amigo">
               <label htmlFor="email">Email</label>
@@ -22,21 +23,31 @@ export default function Formulario() {
               <input type="password" name="password" id="password" />
               <i className="bx bx-hide fa-eye"></i>
             </div>
-            <div className="form-link">
-              <a href="#" className="olvidaste-password">
-                ¿Has olvidado tu contraseña
-              </a>
-            </div>
-            <div className="formulario-input">
-              <button className="amigo btn-amigo">Iniciar sección</button>
-            </div>
-            <div className="form-link">
-              <span>No tiene una cuenta</span>
-              <a href="#" className="">
-                ¿Has olvidado tu contraseña
-              </a>
+            <div className="amigo btn-amigo">
+              <button>Inscribirse</button>
             </div>
           </form>
+          <div className="form-link">
+            <span>
+              ¿Ya tiene una cuenta?
+              <a href="#" className="link inscrirse-link">
+                Iniciar sección
+              </a>
+            </span>
+          </div>
+        </div>
+        <div className="linea"></div>
+        <div className="media-opcion">
+          <a href="#" className="amigo facebbok">
+            <i className="bx bxl-facebook-icon"></i>
+            <span>Iniciar sección con Facebook</span>
+          </a>
+        </div>
+        <div className="media-opcion">
+          <a href="#" className="amigo google">
+            <i className="bx bxl-google"></i>
+            <span>Iniciar sección con Google</span>
+          </a>
         </div>
       </div>
     </section>

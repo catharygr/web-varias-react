510af62b4df187c01f0cc15ef1935933b65490f1
catharygr <103951447+catharygr@users.noreply.github.com>
2023-08-23 18:29:15 +0100

    Trabajando con el componente RopaBase y CSS

--- src/componentes/RopaBase.jsx
+++ src/componentes/RopaBase.jsx
@@ -3,18 +3,34 @@ import "./RopaBase.css";
 export default function RopaBase() {
   return (
     <main className="container">
-      <nav className="nav-logo">
-        <img
-          src="/assets/imagenes/ropa-base/logo.svg"
-          alt="Logo de la empresa"
-        />
-      </nav>
+      <div>
+        <nav className="nav-logo">
+          <img
+            className="logo"
+            src="/assets/imagenes/ropa-base/logo.svg"
+            alt="Logo de la empresa"
+          />
+        </nav>
+        <h1>We are coming soon</h1>
+      </div>
       <section>
-        <img
-          className="hero-mobile"
-          src="/assets/imagenes/ropa-base/hero-mobile.jpg"
-          alt="Imagen de una modelo"
-        />
+        <div className="hero-mobile">
+          <picture>
+            <source
+              media="(max-width: 41.999 )"
+              srcSet="/assets/imagenes/ropa-base/hero-mobile.jpg"
+            />
+            <source
+              media="(min-width: 42rem )"
+              srcSet="/assets/imagenes/ropa-base/hero-desktop.jpg"
+            />
+            <img
+              className="hero"
+              src="/assets/imagenes/ropa-base/hero-mobile.jpg"
+              alt="Imagen de la empresa"
+            />
+          </picture>
+        </div>
       </section>
     </main>
   );

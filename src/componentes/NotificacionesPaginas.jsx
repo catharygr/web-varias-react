e300b445130b87db4463b5ba60d6a22978fd459e
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-10 14:19:09 +0100

    Terminada con JSX

--- src/componentes/NotificacionesPaginas.jsx
+++ src/componentes/NotificacionesPaginas.jsx
@@ -4,111 +4,118 @@ export default function NotificacionesPaginas() {
   return (
     <main className="main-block">
       <header className="block-header">
-        <span className="block-header__notifications">
-          <span className="block-header__notifications-text">
+        <span className="block-header__notificaticaciones">
+          <span className="block-header__notificatificaciones-text">
             Notificaciones
           </span>
-          <span className="block-header__notifications-number">3</span>
+          <span className="block-header__notificatificaciones-numero">3</span>
         </span>
-        <span className="block-header__markall">Marcar todo como leído.</span>
+        <span className="block-header__marcar-todo">
+          Marcar todo como leído.
+        </span>
       </header>
 
-      <div className="block-notifications__type01 block-notifications--notread">
-        <div className="user-img-01"></div>
+      <div className="block-notificatificaciones__tipo01 block-notificaciones--no-leido">
+        <div className="usar-img-01"></div>
         <div>
           <p>
-            <span className="user-name">Mark Webber</span>
-            <span className="user-action"> reacted to your recent post </span>
-            <span className="user-post">My first tournament today!</span>
-            <span className="red-mark"></span>
+            <span className="usar-nombre">Mark Webber</span>
+            <span className="usar-accion">
+              Reaccionó a tu publicación reciente.{" "}
+            </span>
+            <span className="usar-post">¡Mi primer torneo hoy!</span>
+            <span className="red-marcar"></span>
           </p>
-          <p className="user-date">1m ago</p>
+          <p className="usar-dato">Hace 1 minuto.</p>
         </div>
       </div>
 
-      <div className="block-notifications__type01 block-notifications--notread">
-        <div className="user-img-02"></div>
+      <div className="block-notificatificaciones__tipo01 block-notificaciones--no-leido">
+        <div className="usar-img-02"></div>
         <div>
           <p>
-            <span className="user-name">Angela Gray</span>
-            <span className="user-action"> followed you</span>
-            <span className="red-mark"></span>
+            <span className="usar-nombre">Angela Gray</span>
+            <span className="usar-accion">Te ha seguido</span>
+            <span className="red-marcar"></span>
           </p>
-          <p className="user-date">5m ago</p>
+          <p className="usar-dato">Hace 5 minuto.</p>
         </div>
       </div>
 
-      <div className="block-notifications__type01 block-notifications--notread">
-        <div className="user-img-03"></div>
+      <div className="block-notificatificaciones__tipo01 block-notificaciones--no-leido">
+        <div className="usar-img-03"></div>
         <div>
           <p>
-            <span className="user-name">Jacob Thompson</span>
-            <span className="user-action"> has joined your group </span>
-            <span className="user-group">Chess Club</span>
-            <span className="red-mark"></span>
+            <span className="usar-nombre">Jacob Thompson</span>
+            <span className="usar-accion"> Se ha unido a tu grupo. </span>
+            <span className="usar-grupo">Club de ajedrez.</span>
+            <span className="red-marcar"></span>
           </p>
-          <p className="user-date">1 day ago</p>
+          <p className="usar-dato">Hace 1 día</p>
         </div>
       </div>
 
-      <div className="block-notifications__type01 block-notifications--read">
-        <div className="user-img-04"></div>
+      <div className="block-notificatificaciones__tipo01 block-notificaciones--read">
+        <div className="usar-img-04"></div>
         <div>
           <p>
-            <span className="user-name">Rizky Hasanuddin</span>
-            <span className="user-action"> sent you a private message</span>
-            <span className="red-mark"></span>
+            <span className="usar-nombre">Rizky Hasanuddin</span>
+            <span className="usar-accion"> Te envió un mensaje privado.</span>
+            <span className="red-marcar"></span>
           </p>
-          <p className="user-date">5 days ago</p>
+          <p className="usar-dato">Hace 5 días</p>
         </div>
 
         <div className="block-notifications__type02-c">
-          <div className="block-notifications__type02 block-notifications--read">
-            Hello, thanks for setting up the Chess Club. I've been a member for
-            a few weeks now and I am already having lots of fun and improving my
-            game.
+          <div className="block-notifications__type02 block-notificaciones--read">
+            Hola, gracias por crear el Club de Ajedrez. Soy miembro desde hace
+            algunas semanas y ya estoy divirtiéndome mucho y mejorando mi juego.
           </div>
         </div>
       </div>
 
-      <div className="block-notifications__type01 block-notifications--read">
-        <div className="user-img-05"></div>
+      <div className="block-notificatificaciones__tipo01 block-notificaciones--read">
+        <div className="usar-img-05"></div>
         <div>
           <p>
-            <span className="user-name">Kimberly Smith</span>
-            <span className="user-action"> commented on your picture</span>
-            <span className="red-mark"></span>
+            <span className="usar-nombre">Kimberly Smith</span>
+            <span className="usar-accion"> Comentó en tu foto.</span>
+            <span className="red-marcar"></span>
           </p>
-          <p className="user-date">1 week ago</p>
+          <p className="usar-dato">Hace 1 semana.</p>
         </div>
-        <div className="user-img-08"></div>
+        <div className="usar-img-08"></div>
       </div>
 
-      <div className="block-notifications__type01 block-notifications--read">
-        <div className="user-img-06"></div>
+      <div className="block-notificatificaciones__tipo01 block-notificaciones--read">
+        <div className="usar-img-06"></div>
         <div>
           <div>
-            <span className="user-name">Nathan Peterson</span>
-            <span className="user-action"> reacted to your recent post </span>
-            <span className="user-post">
-              5 end-game strategies to increase your win rate
+            <span className="usar-nombre">Nathan Peterson</span>
+            <span className="usar-accion">
+              {" "}
+              Reaccionó a tu publicación reciente.{" "}
             </span>
-            <span className="red-mark"></span>
-            <p className="user-date">2 weeks ago</p>
+            <span className="usar-post">
+              5 estrategias de final de partida para aumentar tu tasa de
+              victoria.
+            </span>
+            <span className="red-marcar"></span>
+            <p className="usar-dato">Hace 2 semanas</p>
           </div>
         </div>
       </div>
 
-      <div className="block-notifications__type01 block-notifications--read">
-        <div className="user-img-07"></div>
+      <div className="block-notificatificaciones__tipo01 block-notificaciones--read">
+        <div className="usar-img-07"></div>
         <div>
           <p>
-            <span className="user-name">Anna Kim</span>
-            <span className="user-action"> left the group </span>
-            <span className="user-group">Chess Club</span>
-            <span className="red-mark"></span>
+            <span className="usar-nombre">Anna Kim</span>
+            <span className="usar-accion"> Salió del grupo </span>
+            <span className="usar-grupo">Club de ajedrez.</span>
+            <span className="red-marcar"></span>
           </p>
-          <p className="user-date">2 weeks ago</p>
+          <p className="usar-dato">Hace 2 semanas</p>
         </div>
       </div>
     </main>

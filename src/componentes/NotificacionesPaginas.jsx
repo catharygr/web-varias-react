c743d984fd335381890c9763a25d5ce12d8f111f
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-14 17:07:53 +0100

    Trabajando con la lógica de la App

--- src/componentes/NotificacionesPaginas.jsx
+++ src/componentes/NotificacionesPaginas.jsx
@@ -1,5 +1,63 @@
 import "./NotificacionesPaginas.css";
 
+const notificacionesData = [
+  {
+    id: 1,
+    nombre: "Mark Webber",
+    accion: "reaccionó a tu publicación reciente.",
+    post: "¡Mi primer torneo hoy!",
+    tiempo: "Hace 1 minuto",
+    leido: false,
+  },
+  {
+    id: 2,
+    nombre: "Angela Gray",
+    accion: "te ha seguido",
+    tiempo: "Hace 5 minutos",
+    leido: false,
+  },
+  {
+    id: 3,
+    nombre: "Jacob Thompson",
+    accion: "se ha unido a tu grupo.",
+    grupo: "Club de ajedrez",
+    tiempo: "Hace 1 día",
+    leido: false,
+  },
+  {
+    id: 4,
+    nombre: "Rizky Hasanuddin",
+    accion: "te envió un mensaje privado.",
+    tiempo: "Hace 5 días",
+    leido: true,
+    mensajePrivado:
+      "Hola, gracias por crear el Club de Ajedrez. Soy miembro desde hace algunas semanas y ya estoy divirtiéndome mucho y mejorando mi juego.",
+  },
+  {
+    id: 5,
+    nombre: "Kimberly Smith",
+    accion: "comentó en tu foto.",
+    tiempo: "Hace 1 semana",
+    leido: true,
+  },
+  {
+    id: 6,
+    nombre: "Nathan Peterson",
+    accion: "reaccionó a tu publicación reciente.",
+    post: "5 estrategias de final de partida para aumentar tu tasa de victoria.",
+    tiempo: "Hace 2 semanas",
+    leido: true,
+  },
+  {
+    id: 7,
+    nombre: "Anna Kim",
+    accion: "salió del grupo",
+    grupo: "Club de ajedrez",
+    tiempo: "Hace 2 semanas",
+    leido: true,
+  },
+];
+
 export default function NotificacionesPaginas() {
   return (
     <main className="main-block">

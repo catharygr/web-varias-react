c917619ee2d755112fe306bb2687468963ccde17
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-14 17:33:32 +0100

    Trabajando con la lógica de la App

--- src/componentes/NotificacionesPaginas.jsx
+++ src/componentes/NotificacionesPaginas.jsx
@@ -71,9 +71,13 @@ export default function NotificacionesPaginas() {
         <span className="block-header__marcar-todo">
           Marcar todo como leído
         </span>
-      </header>
+      </header>   
 
-      <div className="block-notificaciones__tipo01 block-notificaciones--no-leido">
+      {notificacionesData.map((notificacion) => (
+
+      <div  key={notificacion.id} className={`block-notificaciones__tipo01 ${
+        notificacion.leido ? "block-notificaciones--read" : "block-notificaciones--no-leido"
+      }`}>
         <div className="usar-img-01"></div>
         <div>
           <p>
@@ -176,6 +180,117 @@ export default function NotificacionesPaginas() {
           <p className="usar-dato">Hace 2 semanas</p>
         </div>
       </div>
-    </main>
+))}
+  </main>
   );
 }
+
+// import React from "react";
+// import "./NotificacionesPaginas.css";
+
+// const notificacionesData = [
+//   {
+//     id: 1,
+//     nombre: "Mark Webber",
+//     accion: "reaccionó a tu publicación reciente.",
+//     post: "¡Mi primer torneo hoy!",
+//     tiempo: "Hace 1 minuto",
+//     leido: false,
+//   },
+//   {
+//     id: 2,
+//     nombre: "Angela Gray",
+//     accion: "te ha seguido",
+//     tiempo: "Hace 5 minutos",
+//     leido: false,
+//   },
+//   {
+//     id: 3,
+//     nombre: "Jacob Thompson",
+//     accion: "se ha unido a tu grupo.",
+//     grupo: "Club de ajedrez",
+//     tiempo: "Hace 1 día",
+//     leido: false,
+//   },
+//   {
+//     id: 4,
+//     nombre: "Rizky Hasanuddin",
+//     accion: "te envió un mensaje privado.",
+//     tiempo: "Hace 5 días",
+//     leido: true,
+//     mensajePrivado: "Hola, gracias por crear el Club de Ajedrez. Soy miembro desde hace algunas semanas y ya estoy divirtiéndome mucho y mejorando mi juego.",
+//   },
+//   {
+//     id: 5,
+//     nombre: "Kimberly Smith",
+//     accion: "comentó en tu foto.",
+//     tiempo: "Hace 1 semana",
+//     leido: true,
+//   },
+//   {
+//     id: 6,
+//     nombre: "Nathan Peterson",
+//     accion: "reaccionó a tu publicación reciente.",
+//     post: "5 estrategias de final de partida para aumentar tu tasa de victoria.",
+//     tiempo: "Hace 2 semanas",
+//     leido: true,
+//   },
+//   {
+//     id: 7,
+//     nombre: "Anna Kim",
+//     accion: "salió del grupo",
+//     grupo: "Club de ajedrez",
+//     tiempo: "Hace 2 semanas",
+//     leido: true,
+//   },
+// ];
+
+// export default function NotificacionesPaginas() {
+//   return (
+//     <main className="main-block">
+//       <header className="block-header">
+//         <span className="block-header__notificaciones">
+//           <span className="block-header__notificaciones-texto">
+//             Notificaciones{" "}
+//           </span>
+//           <span className="block-header__notificaciones-numero">
+//             {notificacionesData.filter((notificacion) => !notificacion.leido).length}
+//           </span>
+//         </span>
+//         <span className="block-header__marcar-todo">Marcar todo como leído</span>
+//       </header>
+
+//       {notificacionesData.map((notificacion) => (
+//         <div
+//           key={notificacion.id}
+          // className={`block-notificaciones__tipo01 ${
+          //   notificacion.leido ? "block-notificaciones--read" : "block-notificaciones--no-leido"
+          // }`}
+//         >
+//           <div className={`usar-img-${notificacion.id}`}></div>
+//           <div>
+//             <p>
+//               <span className="usar-nombre">{notificacion.nombre} </span>
+//               <span className="usar-accion">{notificacion.accion}</span>
+//               {notificacion.post && (
+//                 <span className="usar-post">{notificacion.post}</span>
+//               )}
+//               {notificacion.grupo && (
+//                 <span className="usar-grupo">{notificacion.grupo}</span>
+//               )}
+//               <span className="red-marcar"></span>
+//             </p>
+//             <p className="usar-dato">{notificacion.tiempo}</p>
+//             {notificacion.mensajePrivado && (
+//               <div className="block-notifications__type02-c">
+//                 <div className="block-notifications__type02">
+//                   {notificacion.mensajePrivado}
+//                 </div>
+//               </div>
+//             )}
+//           </div>
+//         </div>
+//       ))}
+//     </main>
+//   );
+// }

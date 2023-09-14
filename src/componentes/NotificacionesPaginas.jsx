dcfb51f646b3117e02f517d876662d63a3b66e01
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-14 14:35:10 +0100

    Trabajando con JSX

--- src/componentes/NotificacionesPaginas.jsx
+++ src/componentes/NotificacionesPaginas.jsx
@@ -10,16 +10,18 @@ export default function NotificacionesPaginas() {
           </span>
           <span className="block-header__notificatificaciones-numero">3</span>
         </span>
-        <span className="block-header__marcar-todo">Marcar todo com leído</span>
+        <span className="block-header__marcar-todo">
+          Marcar todo como leído
+        </span>
       </header>
 
       <div className="block-notificatificaciones__tipo01 block-notificaciones--no-leido">
         <div className="usar-img-01"></div>
         <div>
           <p>
-            <span className="usar-nombre">Mark Webber</span>
+            <span className="usar-nombre">Mark Webber </span>
             <span className="usar-accion">
-              Reaccionó a tu publicación reciente.{" "}
+              reaccionó a tu publicación reciente.{" "}
             </span>
             <span className="usar-post">¡Mi primer torneo hoy!</span>
             <span className="red-marcar"></span>
@@ -32,8 +34,8 @@ export default function NotificacionesPaginas() {
         <div className="usar-img-02"></div>
         <div>
           <p>
-            <span className="usar-nombre">Angela Gray</span>
-            <span className="usar-accion">Te ha seguido</span>
+            <span className="usar-nombre">Angela Gray </span>
+            <span className="usar-accion">te ha seguido</span>
             <span className="red-marcar"></span>
           </p>
           <p className="usar-dato">Hace 5 minuto.</p>
@@ -44,8 +46,8 @@ export default function NotificacionesPaginas() {
         <div className="usar-img-03"></div>
         <div>
           <p>
-            <span className="usar-nombre">Jacob Thompson</span>
-            <span className="usar-accion"> Se ha unido a tu grupo. </span>
+            <span className="usar-nombre">Jacob Thompson </span>
+            <span className="usar-accion"> se ha unido a tu grupo. </span>
             <span className="usar-grupo">Club de ajedrez.</span>
             <span className="red-marcar"></span>
           </p>
@@ -57,8 +59,8 @@ export default function NotificacionesPaginas() {
         <div className="usar-img-04"></div>
         <div>
           <p>
-            <span className="usar-nombre">Rizky Hasanuddin</span>
-            <span className="usar-accion"> Te envió un mensaje privado.</span>
+            <span className="usar-nombre">Rizky Hasanuddin </span>
+            <span className="usar-accion"> te envió un mensaje privado.</span>
             <span className="red-marcar"></span>
           </p>
           <p className="usar-dato">Hace 5 días</p>
@@ -76,8 +78,8 @@ export default function NotificacionesPaginas() {
         <div className="usar-img-05"></div>
         <div>
           <p>
-            <span className="usar-nombre">Kimberly Smith</span>
-            <span className="usar-accion"> Comentó en tu foto.</span>
+            <span className="usar-nombre">Kimberly Smith </span>
+            <span className="usar-accion"> comentó en tu foto.</span>
             <span className="red-marcar"></span>
           </p>
           <p className="usar-dato">Hace 1 semana.</p>
@@ -89,10 +91,10 @@ export default function NotificacionesPaginas() {
         <div className="usar-img-06"></div>
         <div>
           <div>
-            <span className="usar-nombre">Nathan Peterson</span>
+            <span className="usar-nombre">Nathan Peterson </span>
             <span className="usar-accion">
               {" "}
-              Reaccionó a tu publicación reciente.{" "}
+              reaccionó a tu publicación reciente.{" "}
             </span>
             <span className="usar-post">
               5 estrategias de final de partida para aumentar tu tasa de
@@ -108,8 +110,8 @@ export default function NotificacionesPaginas() {
         <div className="usar-img-07"></div>
         <div>
           <p>
-            <span className="usar-nombre">Anna Kim</span>
-            <span className="usar-accion"> Salió del grupo </span>
+            <span className="usar-nombre">Anna Kim </span>
+            <span className="usar-accion"> salió del grupo </span>
             <span className="usar-grupo">Club de ajedrez.</span>
             <span className="red-marcar"></span>
           </p>

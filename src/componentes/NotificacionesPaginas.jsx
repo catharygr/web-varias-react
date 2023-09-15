d6bb1e31f03be48529a576290472772438824fe7
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-15 15:37:48 +0100

    Trabajando con la imagen comentada

--- src/componentes/NotificacionesPaginas.jsx
+++ src/componentes/NotificacionesPaginas.jsx
@@ -39,7 +39,7 @@ const notificacionesData = [
     accion: "comentó en tu foto.",
     tiempo: "Hace 1 semana",
     leido: true,
-    fotoURL: "08",
+    fotoURL: "foto-comentada",
   },
   {
     id: "06",
@@ -98,6 +98,13 @@ export default function NotificacionesPaginas() {
             </p>
             <p className="usar-dato">{notificacion.tiempo}</p>
           </div>
+          {notificacion.fotoURL && (
+            <img
+              src={notificacion.fotoURL}
+              alt="Foto comentada"
+              className="foto-comentada"
+            />
+          )}
           {notificacion.mensajePrivado && (
             <div className="block-notifications__type02-c">
               <div className="block-notifications__type02 block-notificaciones--read">
@@ -109,110 +116,4 @@ export default function NotificacionesPaginas() {
       ))}
     </main>
   );
-
-  //     <div className="block-notificaciones__tipo01 block-notificaciones--no-leido">
-  //       <div className="usar-img-01"></div>
-  //       <div>
-  //         <p>
-  //           <span className="usar-nombre">Mark Webber </span>
-  //           <span className="usar-accion">
-  //             reaccionó a tu publicación reciente.{" "}
-  //           </span>
-  //           <span className="usar-post">¡Mi primer torneo hoy!</span>
-  //           <span className="red-marcar"></span>
-  //         </p>
-  //         <p className="usar-dato">Hace 1 minuto.</p>
-  //       </div>
-  //     </div>
-
-  //     <div className="block-notificaciones__tipo01 block-notificaciones--no-leido">
-  //       <div className="usar-img-02"></div>
-  //       <div>
-  //         <p>
-  //           <span className="usar-nombre">Angela Gray </span>
-  //           <span className="usar-accion">te ha seguido</span>
-  //           <span className="red-marcar"></span>
-  //         </p>
-  //         <p className="usar-dato">Hace 5 minuto.</p>
-  //       </div>
-  //     </div>
-
-  //     <div className="block-notificaciones__tipo01 block-notificaciones--no-leido">
-  //       <div className="usar-img-03"></div>
-  //       <div>
-  //         <p>
-  //           <span className="usar-nombre">Jacob Thompson </span>
-  //           <span className="usar-accion"> se ha unido a tu grupo. </span>
-  //           <span className="usar-grupo">Club de ajedrez.</span>
-  //           <span className="red-marcar"></span>
-  //         </p>
-  //         <p className="usar-dato">Hace 1 día</p>
-  //       </div>
-  //     </div>
-
-  //     <div className="block-notificaciones__tipo01 block-notificaciones--read">
-  //       <div className="usar-img-04"></div>
-  //       <div>
-  //         <p>
-  //           <span className="usar-nombre">Rizky Hasanuddin </span>
-  //           <span className="usar-accion"> te envió un mensaje privado.</span>
-  //           <span className="red-marcar"></span>
-  //         </p>
-  //         <p className="usar-dato">Hace 5 días</p>
-  //       </div>
-
-  //       <div className="block-notifications__type02-c">
-  //         <div className="block-notifications__type02 block-notificaciones--read">
-  //           Hola, gracias por crear el Club de Ajedrez. Soy miembro desde hace
-  //           algunas semanas y ya estoy divirtiéndome mucho y mejorando mi juego.
-  //         </div>
-  //       </div>
-  //     </div>
-
-  // <div className="block-notificaciones__tipo01 block-notificaciones--read">
-  //   <div className="usar-img-05"></div>
-  //   <div>
-  //     <p>
-  //       <span className="usar-nombre">Kimberly Smith </span>
-  //       <span className="usar-accion"> comentó en tu foto.</span>
-  //       <span className="red-marcar"></span>
-  //     </p>
-  //     <p className="usar-dato">Hace 1 semana.</p>
-  //   </div>
-  //   <div className="usar-img-08"></div>
-  // </div>;
-
-  //     <div className="block-notificaciones__tipo01 block-notificaciones--read">
-  //       <div className="usar-img-06"></div>
-  //       <div>
-  //         <div>
-  //           <span className="usar-nombre">Nathan Peterson </span>
-  //           <span className="usar-accion">
-  //             {" "}
-  //             reaccionó a tu publicación reciente.{" "}
-  //           </span>
-  //           <span className="usar-post">
-  //             5 estrategias de final de partida para aumentar tu tasa de
-  //             victoria.
-  //           </span>
-  //           <span className="red-marcar"></span>
-  //           <p className="usar-dato">Hace 2 semanas</p>
-  //         </div>
-  //       </div>
-  //     </div>
-
-  //     <div className="block-notificaciones__tipo01 block-notificaciones--read">
-  //       <div className="usar-img-07"></div>
-  //       <div>
-  //         <p>
-  //           <span className="usar-nombre">Anna Kim </span>
-  //           <span className="usar-accion"> salió del grupo </span>
-  //           <span className="usar-grupo">Club de ajedrez.</span>
-  //           <span className="red-marcar"></span>
-  //         </p>
-  //         <p className="usar-dato">Hace 2 semanas</p>
-  //       </div>
-  //     </div>
-  //   </main>
-  // );
 }
